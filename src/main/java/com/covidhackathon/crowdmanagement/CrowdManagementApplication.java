210cb695a8557cb0b19fddfb776ed82354853499
Rahul Swami <swamirahul10@gmail.com>
2020-04-10 15:03:40 +0530

    create skeleton

---
+++ src/main/java/com/covidhackathon/crowdmanagement/CrowdManagementApplication.java
@@ -0,0 +1,13 @@
+package com.covidhackathon.crowdmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdManagementApplication.class, args);
+	}
+
+}
